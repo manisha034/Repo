4915fd9038e1afd24b85956021526ebc7cd6f122
manisha034 <97963856+manisha034@users.noreply.github.com>
2025-11-06 17:01:48 +0530

    updated java code by dev2
    Added a new Main class that calculates the sum of two numbers.

--- app.java
+++ app.java
@@ -3,3 +3,17 @@ public class HelloWorld {
         System.out.println("Hello, World!");
     }
 }
+
+//dev-2 code push from local
+class Main {
+
+  public static void main(String[] args) {
+    
+    int first = 10;
+    int second = 20;
+
+    // add two numbers
+    int sum = first + second;
+    System.out.println(first + " + " + second + " = "  + sum);
+  }
+}
